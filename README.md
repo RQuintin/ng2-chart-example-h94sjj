06f32005350d5859efb3bac0a56786b0c8bf7024
RQuintin <quintinruggiero@hotmail.com>
2021-03-06 15:35:53 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ng2-chart-example-fxqafe
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ng2-chart-example-fxqafe)
---
+++ app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+
+
+import { ChartsModule } from 'ng2-charts';
+
+@NgModule({
+  imports:      [ BrowserModule, HttpClientModule, FormsModule, ChartsModule ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
